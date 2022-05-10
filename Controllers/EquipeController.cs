6f5f57c7f64108a9f42356351f4e99d866b5a413
mmsansao <64363954+mmsansao@users.noreply.github.com>
2022-05-10 20:56:19 -0300

    criação do db

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 ﻿using EJOGOS.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EJOGOS.Controllers
@@ -20,6 +21,27 @@ namespace EJOGOS.Controllers
 
 
             return View();
+        }
+
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+
+            // criar um objeto equipe a partir do frontend
+            // seria o IFormCollection
+            Equipe novaEquipe = new Equipe();
+
+            novaEquipe.idEquipe = int.Parse(form["IdEquipe"]); // a string do form é o que vai aparecer na página, vai voltar uma string que deve ser convertida em int
+            novaEquipe.Nome = form["Nome"];
+            novaEquipe.Imagem = form["Imagem"];
+
+            //chamar a função CRIAR passando um objeto do tipo Equipe
+            equipeModel.Criar(novaEquipe);
+
+            // ele deve então atualizar a viewbag com a equipe nova
+            ViewBag.Equipes = equipeModel.LerEquipes();
+
+            return LocalRedirect("~/Equipe");
+
         }
     }
 }
